5ff1d07560ecb5107ef34bdb167da04d1f4d8c40
Boris Kliushyn <kljushin@gmail.com>
2016-06-14 20:34:26 +0300

    add Pow func

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 var prompt = require('prompt');
 var powFunc = require('./lib/myPow.js');
 
-prompt.start();
-prompt.get(['base', 'exponent'], function (err, result) {
-    console.log('Resault is ', powFunc(result.base,result.exponent));
-});
+function Pow() {
+    prompt.start();
+    prompt.get(['base', 'exponent'], function (err, result) {
+        console.log('Resault is ', powFunc(result.base, result.exponent));
+    });
+}
 
+module.exports = Pow;
