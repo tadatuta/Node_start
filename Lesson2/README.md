9190e3b8080c726baef846a1df071c59dda8aa6d
Boris Kliushyn <kljushin@gmail.com>
2016-06-21 22:29:21 +0300

    add README.md

---
+++ Lesson2/README.md
@@ -0,0 +1,9 @@
+# Скрипт принимает данные из коммандной строки
+
+## Пример данных для скрипта
+
+1. http://localhost:3000/?url=http://rozetka.com.ua&count=200&selector=.g-title
+
+2. http://localhost:3000/?url=https://dou.ua/&count=100&selector=.comment
+
+3. http://localhost:3000/?url=http://webscraper.io/&count=10&selector=.caption
--- Lesson2/index.js
+++ Lesson2/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const tress = require('tress');
 const cheerio = require('cheerio');
 const fs = require('fs');
@@ -7,39 +9,68 @@ const port = 3000;
 const qs = require('querystring');
 const url = require('url');
 const needle = require('needle');
-const URL = 'https://www.microsoft.com/uk-ua/';
-const SELECTOR = '.description';
-var links = [];
-var results = [];
+const URL = 'http://webscraper.io/test-sites/e-commerce/more';
+const SELECTOR = '.caption';
+
 
 const server = app.get('/', function (req, res) {
     const urlObj = url.parse(req.url);
     const query = qs.parse(urlObj.query);
-    var hostname = url.parse(query.url).hostname;
+    const hostname = url.parse(query.url).hostname;
+    const fullHostname = url.parse(query.url).protocol + '//' + hostname;
+    const selector = query.selector;
     
-    needle.get(query.url, function(err, data){
-        if (err) throw err;
-        $ = cheerio.load(data.body);
-        $(SELECTOR).each(function () {
-           results.push({
-               url: query.url,
-               content: $(this).text()
-           });
-        });
-        $('a').each(function () {
-            var link = $(this).attr('href');
-            if(url.parse(link).hostname == hostname) {
-                links.push(link);
-            }
-        });
-        console.log('results',results);
-        console.log(results.length);
-        res.end(data.body);
-        
-    });
-   
-    fs.writeFileSync('./result.json', JSON.stringify(results));
+    let links = [];
+    let results = [];
+    let counter = query.count;
+    
 
+    console.log(`URL: ${query.url}  LINKS NUMBER: ${counter}    SELECTOR: ${selector}`);
+   
+ 
+    
+    const q = tress(function (nextUrl, callback) {
+        needle.get(nextUrl, function(err, data){
+                if (err) throw err;
+                const $ = cheerio.load(data.body);
+                console.log(nextUrl);
+                $(selector).each(function () {
+                    results.push({
+                        url: nextUrl,
+                        content: $(this).text()
+                    });
+                });
+                $('a').each(function () {
+                    var link = $(this).attr('href');
+                    //console.log(`link: ${link} url: ${nextUrl}`);
+                    if(link != undefined) {
+                        if (link[0] == '/') {
+                            link = url.resolve(fullHostname, link);
+                        }
+                        //console.log(`link: ${link} url: ${nextUrl}`);
+                        if (!links[link] && url.parse(link).hostname == hostname && url.parse(link).protocol != 'mailto:') {
+                            links[link] = 1;
+                            if (counter > 0) {
+                                counter--;
+                                q.push(link);
+                            }
+                        }
+                    }
+                });
+
+                callback();
+                
+        });
+    }, 100);
+
+    q.drain = function(){
+        res.send('DONE');
+        fs.writeFileSync('./data.json', JSON.stringify(results, null, 4));
+        console.log('Task completed')
+    };
+    
+    links[query.url] = 1;
+    q.push(query.url);
 });
 
 
