1a9a855dfcbb934a893d61722169c4222a90aeb7
Vladimir Grinenko <i@tadatuta.com>
2016-07-21 01:22:40 +0300

    review

--- Lesson2/test/test.js
+++ Lesson2/test/test.js
@@ -3,13 +3,13 @@ const cli = require('../bin/cli.js');
 
 describe('Console work test',function () {
     describe('run without args',function () {
-        
+
         it('should be help massage',function () {
             const expected = cli.options;
-            const resault = cli.usage;
-            
-            assert.equal(expected,resault);
+            const result = cli.usage;
+
+            assert.equal(expected, result);
         });
     });
-    
+
 });
